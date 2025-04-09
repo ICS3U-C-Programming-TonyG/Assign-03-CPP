22baeef73a1ca2e8a2d686e108d4d96a2bee2671
TONY GAO <TONY.GAO@STU.OCSB.CA>
2025-04-09 18:12:31 +0000

    no error cpplint, version 11 C++

--- assign-03.cpp
+++ assign-03.cpp
@@ -1,14 +1,11 @@
 // Copyright (c) 2020 Tony G All rights reserved.
-
+//
 // Created by: Tony G
-
 // Created on: 2025-03-17
-
 // Converts upper to lowercase and lower to uppercase, also tells you
 // which case it is.
 
-#include <cctype>  // for isalpha, isupper, islower, toupper, tolower
-// cctype learned from https://www.programiz.com/cpp-programming/library-function/cctype
+#include <cctype>  // cctype learned from https://www.programiz.com/cpp-programming/library-function/cctype
 #include <iostream>
 #include <string>
 
@@ -20,7 +17,7 @@ int main() {
         std::cout << "Enter a letter (or type 'exit' to quit): ";
         std::cin >> userInput;
 
-        // Exit condition. Break learned from
+        // Exit condition. Break learned from:
         // https://www.w3schools.com/cpp/cpp_while_loop.asp
         if (userInput == "exit" || userInput == "EXIT" || userInput == "Exit") {
             std::cout << "Goodbye." << std::endl;
@@ -28,38 +25,38 @@ int main() {
         }
 
         // Check for valid input (single letter)
-        // .length learned from https://www.w3schools.com/cpp/cpp_strings_length.asp
-        // isalpha learned from https://docs.vultr.com/cpp/standard-library/cctype/isalpha
+        // .length learned from:
+        // https://www.w3schools.com/cpp/cpp_strings_length.asp
+        // isalpha learned from:
+        // https://docs.vultr.com/cpp/standard-library/cctype/isalpha
         if (userInput.length() == 1 && std::isalpha(userInput[0])) {
             char letter = userInput[0];
 
-            // Check uppercase
-            // isupper learned from https://www.programiz.com/cpp-programming/library-function/cctype/isupper
-            // (tolower, toupper and islower all learned from programiz)
+            // isupper learned from:
+            // https://www.programiz.com/cpp-programming/library-function/cctype/isupper
             if (std::isupper(letter)) {
                 std::cout << "The letter you have inputted is uppercase."
                           << std::endl;
                 std::cout << "The lowercase version is: "
-                          << (char)std::tolower(letter) << std::endl;
-            }
-            // Check lowercase
-            // islower and toupper learned from
-            // https://www.programiz.com/cpp-programming/library-function/cctype/islower
-            else if (std::islower(letter)) {
+                          << static_cast<char>(std::tolower(letter))
+                          << std::endl;
+            } else if (std::islower(letter)) {
+                // islower and toupper learned from:
+                // https://www.programiz.com/cpp-programming/library-function/cctype/islower
                 std::cout << "The letter you have inputted is lowercase."
                           << std::endl;
                 std::cout << "The uppercase version is: "
-                          << (char)std::toupper(letter) << std::endl;
+                          << static_cast<char>(std::toupper(letter))
+                          << std::endl;
             }
 
-            // Alphabet half check
-            // tolower learned from
+            // tolower learned from:
             // https://www.programiz.com/cpp-programming/library-function/cctype/tolower
             if (std::tolower(letter) <= 'm') {
                 std::cout << "This letter is in the first half of the alphabet."
                           << std::endl;
             } else {
-                std::cout << "This letter is in the second half of the alphabet."
+                std::cout << "This letter is in the 2nd half of the alphabet."
                           << std::endl;
             }
         } else {
@@ -67,5 +64,4 @@ int main() {
                       << "letter." << std::endl;
         }
     }
-
 }
