8e9dc41ba93f680766a2615f7f91579812f9d1ed
TONY GAO <TONY.GAO@STU.OCSB.CA>
2025-04-09 17:58:46 +0000

    version 5 C++

--- assign-03.cpp
+++ assign-03.cpp
@@ -12,22 +12,22 @@ int main() {
         std::cout << "Enter a letter (or type 'exit' to quit): ";
         std::cin >> userInput;
 
-        // Exit condition. 
+        // Exit condition. Break learned from https://www.w3schools.com/cpp/cpp_while_loop.asp
         if (userInput == "exit" || userInput == "EXIT" || userInput == "Exit") {
             std::cout << "Goodbye." << std::endl;
             break;
         }
 
-        // Check for valid input (single letter) Break learned from https://www.w3schools.com/cpp/cpp_while_loop.asp
+        // Check for valid input (single letter) .length learned from https://www.w3schools.com/cpp/cpp_strings_length.asp isalpha learned from https://docs.vultr.com/cpp/standard-library/cctype/isalpha
         if (userInput.length() == 1 && std::isalpha(userInput[0])) {
             char letter = userInput[0];
 
-            // Check uppercase .length learned from https://www.w3schools.com/cpp/cpp_strings_length.asp isalpha learned from https://docs.vultr.com/cpp/standard-library/cctype/isalpha
+            // Check uppercase. isupper learned from https://www.programiz.com/cpp-programming/library-function/cctype/isupper (tolower, toupper and islower all learned from progamiz)
             if (std::isupper(letter)) {
                 std::cout << "The letter you have inputted is uppercase." << std::endl;
                 std::cout << "The lowercase version is: " << (char)std::tolower(letter) << std::endl;
             }
-            // Check lowercase
+            // Check lowercase isupper learned from 
             else if (std::islower(letter)) {
                 std::cout << "The letter you have inputted is lowercase." << std::endl;
                 std::cout << "The uppercase version is: " << (char)std::toupper(letter) << std::endl;
